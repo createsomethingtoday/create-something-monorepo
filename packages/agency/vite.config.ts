fe28ef99feaaf365ae031e7f2dfcd93a7a0a26a3
Create Something <micah@createsomething.io>
2026-03-07 01:47:58 -0600

    Add ssr resolve conditions to agency

--- packages/agency/vite.config.ts
+++ packages/agency/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		resolve: {
+			conditions: ['workerd', 'worker']
+		}
+	},
 	build: {
 		chunkSizeWarningLimit: 1200
 	}
