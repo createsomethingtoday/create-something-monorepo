786a75f10fa1218c44d7f42c47280b15bd11c2c7
Create Something <micah@createsomething.io>
2026-03-07 01:39:18 -0600

    Add platform env typing to logout

--- packages/ltd/src/routes/api/auth/logout/+server.ts
+++ packages/ltd/src/routes/api/auth/logout/+server.ts
@@ -2,5 +2,13 @@ import { handleLogout } from '@create-something/canon/auth';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ request, cookies, platform }) => {
-	return handleLogout(request, cookies, platform);
+	return handleLogout(
+		request,
+		cookies,
+		platform
+			? {
+					env: platform.env as Record<string, unknown> & { ENVIRONMENT?: string | undefined }
+				}
+			: undefined
+	);
 };
